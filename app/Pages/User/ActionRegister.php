3419dcf627448b772e2f5098e1be4762604d4fcb
DimNS <atomcms@ya.ru>
2016-07-21 13:17:18 +0500

    Добавлена отправка писем при регистрации

--- app/Pages/User/ActionRegister.php
+++ app/Pages/User/ActionRegister.php
@@ -68,9 +68,17 @@ class ActionRegister
                 $user_info->save();
 
                 if (is_object($user_info) AND isset($user_info->id)) {
+                    // Отправим сообщение на почту
+                    $this->di->mail->send($user_email, $user_name . ', добро пожаловать в "' . $this->di->auth->config->site_name . '"', 'user_register', [
+                        '[[SITE_NAME]]'     => $this->di->auth->config->site_name,
+                        '[[SITE_URL]]'      => $this->di->auth->config->site_url,
+                        '[[USER_EMAIL]]'    => $user_email,
+                        '[[USER_PASSWORD]]' => $password,
+                    ]);
+
                     // Войдем под этим пользователем
-                    $login  = new ActionLogin($this->di);
-                    $user = $login->run($user_email, $password);
+                    $login = new ActionLogin($this->di);
+                    $user  = $login->run($user_email, $password);
                 } else {
                     $user['message'] = 'Произошла ошибка при изменении данных. Попробуйте войти ещё раз.';
                 }
