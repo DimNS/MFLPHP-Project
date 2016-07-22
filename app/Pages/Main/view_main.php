649413ba76b036f81475d6ce0cf21769b1ba55e1
DimNS <atomcms@ya.ru>
2016-07-22 14:15:13 +0500

    Новые пункты в меню

--- app/Pages/Main/view_main.php
+++ app/Pages/Main/view_main.php
@@ -1,4 +1,4 @@
-<p><a href="<?=$this->path?>/user/logout">Выход</a></p>
+<p><a href="<?=$this->path?>/">Главная</a> | <a href="<?=$this->path?>/user">Мой профиль</a> | <a href="<?=$this->path?>/user/logout">Выход</a></p>
 <p>dashboard</p>
 <p>Вы вошли как: <?=$this->userinfo->name?> <small>(#<?=$this->userinfo->uid?>, <?=$this->userinfo->access?>)</small></p>
 <p>url: "<?=$this->uri?>"</p>
