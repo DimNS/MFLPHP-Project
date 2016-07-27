cc7beb8190e3309abc350df889b52d8573b6a0ab
DimNS <atomcms@ya.ru>
2016-07-27 09:39:21 +0500

    Добавлены отступы от кнопки

--- app/Views/404.php
+++ app/Views/404.php
@@ -5,7 +5,7 @@
                 <h1 class="error-title">404</h1>
                 <h2 class="error-sub-title">Страница не найдена</h2>
             </div>
-            <p class="text-xs-center">
+            <p class="text-xs-center m-t-3">
                 <a href="<?=$this->path?>/" class="btn btn-primary"><i class="fa fa-angle-left"></i> Назад на главную</a>
             </p>
         </div>
