4ee59c04272b4a8289f98b6f8b97105a456ec1e5
Dmitrii Shcherbakov <d@shinmar.ru>
2016-07-28 13:03:56 +0500

    Код начальных настроек вынесен в свой файл

--- app/Views/layout-default.php
+++ app/Views/layout-default.php
@@ -98,8 +98,9 @@
 
     <!-- modular-admin-html -->
     <script src="<?=$this->path?>/assets/modular-admin-v101/js/vendor.js" type="text/javascript"></script>
-    <script src="<?=$this->path?>/assets/modular-admin-v101/js/app.js" type="text/javascript"></script>
 
+    <script src="<?=$this->path?>/assets/js/config<?php print('1' === getenv('PRODUCTION') ? '.min' : ''); ?>.js?v=<?=md5_file(__DIR__ . '/../../assets/js/config' . ('1' === getenv('PRODUCTION') ? '.min' : '') . '.js')?>" type="text/javascript"></script>
+    <script src="<?=$this->path?>/assets/js/ajax<?php print('1' === getenv('PRODUCTION') ? '.min' : ''); ?>.js?v=<?=md5_file(__DIR__ . '/../../assets/js/ajax' . ('1' === getenv('PRODUCTION') ? '.min' : '') . '.js')?>" type="text/javascript"></script>
     <script src="<?=$this->path?>/assets/js/app<?php print('1' === getenv('PRODUCTION') ? '.min' : ''); ?>.js?v=<?=md5_file(__DIR__ . '/../../assets/js/app' . ('1' === getenv('PRODUCTION') ? '.min' : '') . '.js')?>" type="text/javascript"></script>
 </body>
 </html>
