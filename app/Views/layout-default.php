d1329764b5594d9975a7017bae45ab8fe484f5cf
DimNS <atomcms@ya.ru>
2016-11-16 12:56:03 +0500

    Автоматический расчет даты копирайта

--- app/Views/layout-default.php
+++ app/Views/layout-default.php
@@ -79,8 +79,12 @@
                         <iframe class="footer-github-btn" src="https://ghbtns.com/github-btn.html?user=dimns&repo=mflphp-project&type=star&count=true" frameborder="0" scrolling="0" width="140px" height="20px"></iframe>
                     </div>
                     <div class="footer-block author">
+                        <?php
+                        $begin_year = 2016;
+                        $copy_date = (date('Y') == $begin_year ? $begin_year : $begin_year . '-' . date('Y'));
+                        ?>
                         <ul>
-                            <li>2016 &copy; <a href="https://github.com/dimns">DimNS</a></li>
+                            <li><?=$copy_date?> &copy; <a href="https://github.com/dimns">DimNS</a></li>
                         </ul>
                     </div>
                 </footer>
