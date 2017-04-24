6cf8773f31a94dfdc9a724b95a6649488f5830ce
DimNS <atomcms@ya.ru>
2017-04-24 17:38:23 +0500

    Скрываем блок если нет сообщения

--- app/Views/external-page-header.php
+++ app/Views/external-page-header.php
@@ -1,6 +1,12 @@
 <p>
     <img src="<?=$this->path?>assets/img/logo.png">
 </p>
-<p class="<?=$this->message_code?>">
-    <?=$this->message_text?>
-</p>
+<?php
+if ($this->message_text !== '') {
+    ?>
+    <p class="<?=$this->message_code?>">
+        <?=$this->message_text?>
+    </p>
+    <?php
+}
+?>
